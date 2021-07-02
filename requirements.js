aa6bc08d14852cc20397f44b626ce467d3a2901e
sainaveen7675 <gajula.sainaveen1122@gmail.com>
2021-07-02 19:45:22 +0530

    new project

---
+++ requirements.js
@@ -0,0 +1,38 @@
+// Requirements for our project
+
+
+// We are a book management company
+
+
+// Books
+// ISBN, title, pub date, language, no of pages, author, category
+
+// Authors
+// id, name, books[]
+
+// Publications
+// id, name, books[]
+
+
+// What are all the APIs that we need........
+
+// **Books**
+// We need an API
+// to get all books........ [done]
+// to get specific books ......... [done]
+// to get list of books based on category.......[done]
+// to get list of books based on languages [TASK]
+
+
+// **Authors**
+// We need an API
+// to get all authors.......[done]
+// to get specific authors [TASK]
+// to get list f authors based on books.....[done]
+
+
+// **Publications**
+// We need an API
+// to get all publication.......[done]
+// to get specific publication [TASK]
+// to get list of publications based on book [TASK]
